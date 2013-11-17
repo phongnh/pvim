2c368c9d7a808c166d4cd4dbc0178f26ffb68437
Phong Nguyen <nhphong1406@gmail.com>
2013-11-17 13:21:05 +0700

    Add 'pathogen' command for downloading pathogen from tpope/vim-pathogen repo

--- lib/pvim/cli.rb
+++ lib/pvim/cli.rb
@@ -2,9 +2,16 @@ require 'pvim/version'
 
 module Pvim
   class CLI < Thor
+    include Thor::Actions
+
     desc 'version', 'Prints version'
     def version
       puts Pvim::VERSION
+    end
+
+    desc 'pathogen', 'Get Pathogen from https://github.com/tpope/vim-pathogen'
+    def pathogen
+      get "https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim", "autoload/pathogen.vim"
     end
   end
 end
